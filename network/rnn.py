4ff479a83f05376857403bc08d815c5fdc8fbdbf
whoami0022 <31894199+whoami0022@users.noreply.github.com>
2018-12-21 17:10:13 -0800

    Update rnn.py

--- network/rnn.py
+++ network/rnn.py
@@ -5,14 +5,17 @@ import vocab
 
 
 class RNN(nn.Module):
-    def __init__(self, embedding_dim, hidden_dim, vocab_size, num_layers):
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, num_layers, weights):
         super(RNN, self).__init__()
         self.hidden_dim = hidden_dim
         self.word_embeddings = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embedding_dim)
         self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_dim, num_layers=num_layers)
         self.linear = nn.Linear(in_features=hidden_dim, out_features=vocab_size)
 
-        self.word_embeddings.weight.data.uniform_(-0.1, 0.1)
+        # self.word_embeddings.weight.data.uniform_(-0.1, 0.1)
+        weights = torch.Tensor(weights)
+        print(weights.size())
+        self.word_embeddings.weight = nn.Parameter(weights, requires_grad=False)
         self.linear.weight.data.uniform_(-0.1, 0.1)
         self.linear.bias.data.fill_(0)
 
